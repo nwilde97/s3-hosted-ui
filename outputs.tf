470536611666cd9c45693aac8f92de4124ccab0b
Ned Wilde <ned.wilde@nike.com>
2020-03-06 10:33:08 -0800

    Change outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output RestApiName {
- value = aws_api_gateway_rest_api.RestApi.name
+output RestApiId {
+ value = aws_api_gateway_rest_api.RestApi.id
+}
+
+output RootResourceId {
+ value = aws_api_gateway_rest_api.RestApi.root_resource_id
 }
